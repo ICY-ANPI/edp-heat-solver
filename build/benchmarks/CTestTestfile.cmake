bcac0559d53f28f69caff7b3a79d49cc08cdd945
crisrivlop <crisrivlop@gmail.com>
2018-06-07 14:52:34 -0600

    cofiguration setted

--- build/benchmarks/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cristian/Documents/analisis numerico/eigen-system/benchmarks
-# Build directory: /home/cristian/Documents/analisis numerico/eigen-system/build/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(benchmark "/home/cristian/Documents/analisis numerico/eigen-system/build/benchmarks/benchmark")
--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cristian/Documents/analisis numerico/eigen-system/test
-# Build directory: /home/cristian/Documents/analisis numerico/eigen-system/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tester "/home/cristian/Documents/analisis numerico/eigen-system/build/test/tester")
