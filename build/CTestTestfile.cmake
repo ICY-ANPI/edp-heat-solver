8cf1a55f10aafca40a1ba77dc3e8fd0f798a7dd3
crisrivlop <crisrivlop@gmail.com>
2018-06-07 14:34:34 -0600

    repo inicialization

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cristian/Documents/analisis numerico/eigen-system
+# Build directory: /home/cristian/Documents/analisis numerico/eigen-system/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(src)
+subdirs(test)
+subdirs(benchmarks)
